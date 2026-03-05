1b88a26b403edb4c602edbb1b37cfdf10361e854
Aleksey84e <egoist.2665@gmail.com>
2026-03-05 21:17:11 +0300

    Здача контрольной работы

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,53 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+driver_path= "edge/msedgedriver.exe"
+service = EdgeService(driver_path)
+driver = webdriver.Edge(service=service)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class TestDataTypesForm:
+
+    def get_field_color(self, driver, field_id):
+        field = driver.find_element(By.ID, field_id)
+        return field.value_of_css_property("background-color")
+
+    def test_form_validation_colors(self):
+        self.driver = webdriver.Edge()
+        self.driver.maximize_window()
+        self.wait = WebDriverWait(driver, 10)
+
+        try:
+            driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+            driver.find_element(By.NAME, "first-name").send_keys("Иван")
+            driver.find_element(By.NAME, "last-name").send_keys("Петров")
+            driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+            driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
+            driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
+            driver.find_element(By.NAME, "zip-code").clear()
+            driver.find_element(By.NAME, "city").send_keys("Москва")
+            driver.find_element(By.NAME, "country").send_keys("Россия")
+            driver.find_element(By.NAME, "job-position").send_keys("QA")
+            driver.find_element(By.NAME, "company").send_keys("SkyPro")
+            driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+            self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "alert-success")))
+
+            assert "alert-danger" in driver.find_element(By.ID, "zip-code").get_attribute("class")
+
+            fields = [
+                "first-name", "last-name", "address", "city",
+                "country", "e-mail", "phone", "job-position", "company"
+            ]
+
+            for field_id in fields:
+                assert "alert-success" in driver.find_element(By.ID, field_id).get_attribute("class")
+
+        finally:
+           driver.quit()
+
+if __name__ == "__main__":
+    pytest.main(["-v", "-s", __file__])
---
+++ 06K_lesson/test_03_shop.py
@@ -0,0 +1,48 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+
+def test_saucedemo_checkout():
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service)
+    wait = WebDriverWait(driver, 10)
+
+    try:
+        driver.get("https://www.saucedemo.com/")
+        driver.maximize_window()
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        driver.find_element(By.ID, "login-button").click()
+
+        items = ["add-to-cart-sauce-labs-backpack",
+                 "add-to-cart-sauce-labs-bolt-t-shirt",
+                 "add-to-cart-sauce-labs-onesie"]
+
+        for item_id in items:
+            driver.find_element(By.ID, item_id).click()
+
+        driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+        driver.find_element(By.ID, "checkout").click()
+        driver.find_element(By.ID, "first-name").send_keys("Иван")
+        driver.find_element(By.ID, "last-name").send_keys("Петров")
+        driver.find_element(By.ID, "postal-code").send_keys("123456")
+        driver.find_element(By.ID, "continue").click()
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "summary_total_label")))
+        total_text = driver.find_element(By.CLASS_NAME, "summary_total_label").text
+        total_value = float(total_text.replace("Total: $", ""))
+
+        expected_total = 58.29
+        assert total_value == expected_total, f"Ожидалось {expected_total}, получено {total_value}"
+
+        print(f"✓ Тест пройден! Итоговая сумма: {total_text}")
+
+    finally:
+        driver.quit()
+
+    if __name__ == "__main__":
+        pytest.main(["-v", "-s", __file__])
